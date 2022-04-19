46443a0b694e69491bc09366d740903b30fa3383
theHabesa1 <90505588+theHabesa1@users.noreply.github.com>
2022-04-19 16:02:08 +0300

    not working files an junk deleting

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,64 @@
-import  "./Testimonials.scss"
+import "./Testimonials.scss";
 
 export default function Testimonials() {
+  const data = [
+    {
+      id: 1,
+      name: "Yared Abera",
+      title: "Senior Developer",
+      img:
+        "assets/yared.jpg",
+      icon: "assets/instagram.png",
+      desc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+    },
+    {
+      id: 2,
+      name: "Yabsera Abera",
+      title: "Co-Founder of DELKA",
+      img:
+        "assets/yab.jpg",
+      icon: "assets/telegram.png",
+      desc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+      featured: true,
+    },
+    {
+      id: 3,
+      name: "Meron Hareklm",
+      title: "CEO of ALBI",
+      img:
+        "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/facebook.png",
+      desc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+    },
+  ];
   return (
     <div className="testimonials" id="testimonials">
-    
+      <h1>Testimonials</h1>
+      <div className="container">
+        {data.map((d) => (
+          <div className={d.featured ? "card featured" : "card"}>
+            <div className="top">
+              <img src="assets/right-arrow.png" className="left" alt="" />
+              <img
+                className="user"
+                src={d.img}
+                alt=""
+              />
+              <img className="right" src={d.icon} alt="" />
+            </div>
+            <div className="center">
+              {d.desc}
+            </div>
+            <div className="bottom">
+              <h3>{d.name}</h3>
+              <h4>{d.title}</h4>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
