2d71e7858577da55434cc0c6a5a9fc69aa31df5a
theHabesa1 <90505588+theHabesa1@users.noreply.github.com>
2022-04-21 13:20:40 +0300

    third commit
    a change has been made to contacts

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -1,8 +1,30 @@
 import "./contact.scss"
+import { useState } from "react";
+export default function Contact() {
+  const [message, setMessage] = useState(false);
 
-export default function contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
   return (
+   
     <div className='contact' id="contact">
+    <div className="left">
+      <img src="assets/handshake-friends-svgrepo-com.svg" />
+    </div>
+    <div className="right">
+     
+      <form onSubmit={handleSubmit} className="form">
+      <h2>CONTACT US</h2>
+    <p type="Name:"><input placeholder="Write your name here.."></input></p>
+    <p type="Email:"><input placeholder="Let us know how to contact you back.."></input></p>
+    <p type="Message:"><input placeholder="What would you like to tell us.."></input></p>
+    <button type="submit"> Send Message</button>
+    {message && <span>Thanks, I'll reply ASAP :)</span>}
+  
+      </form>
+    </div>
     
     </div>
   )
